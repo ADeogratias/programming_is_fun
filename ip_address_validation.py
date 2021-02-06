d00c98a99bb278f1bb68e7d2f950b21cee009756
deog.amani@gmail.com <deog.amani@gmail.com>
2021-02-06 12:42:04 +0200

    We are done working on the valid ip address and it is showing all the non valid ip addresses provided.

--- ip_address_validation.py
+++ ip_address_validation.py
@@ -7,3 +7,45 @@
 # // 255.255.255.255
 # // 88.990.001123
 
+# We are going to work on the ip address validation algorithm
+
+def valid_ip(ip):
+    testvar = ip.split('.')
+    # print (testvar[0])
+
+    iplen = len(testvar)
+
+    if iplen < 4 or iplen >4:
+        print(ip + ' Is is not a valid IP address\n')
+        return
+
+    for i in testvar:
+
+        if i.isnumeric() == False:
+            print(ip + ' Is not a valid IP address \n')
+            return
+        i = int(i)
+        if ( i < 0 or i > 255):
+            print( ip + ' Is not a valid IP address \n')
+            return
+
+
+    print("Your input ip: '" + ip + "' is a valid ip address \n")
+
+
+ip = ['12.233.11.2',
+      'asd.adsf.sadf',
+      'asdfadsfadf',
+      '12232.11232.23.32231',
+      '0.0.0.0',
+      '255.255.255.255',
+      '88.990.001123',
+      'asd.adsf.sadf.sadf',
+      '22a.32.32.22',
+      '12.233.11.-2',
+      '12.233.11..2',
+      '12.233.11.',
+      '12.233. 11.2']
+
+for ipad in ip:
+    valid_ip(ipad)
